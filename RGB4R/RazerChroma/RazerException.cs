08b0552ff18d9e4ea0d5af800c27a513a859bcf2
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-19 05:49:55 -0400

    Added exception for Razer messages

---
+++ RGB4R/RazerChroma/RazerException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RGB4R.RazerChroma;
+
+/// <summary>
+/// Exception raised when there is a problem with Razer Synapse.
+/// </summary>
+public class RazerException : Exception
+{
+    /// <summary>
+    /// Creates a new <see cref="RazerException"/>.
+    /// </summary>
+    /// <param name="message">The message of the exception.</param>
+    /// <param name="error">The error code returned by the API.</param>
+    public RazerException(string message, RazerError error) : base($"{message}: Code {error} ({(int)error}).")
+    {
+    }
+}
