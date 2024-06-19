7127b861d31b5df57e91f4ae46a1b8ed91f9df1d
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-19 04:48:30 -0400

    Added basic files

---
+++ RGB4R/Configuration.cs
@@ -0,0 +1,70 @@
+﻿using GTA.UI;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace RGB4R;
+
+/// <summary>
+/// The configuration of the mod.
+/// </summary>
+internal class Configuration
+{
+    #region Fields
+
+    private static readonly string path = Path.ChangeExtension(new Uri(Assembly.GetAssembly(typeof(Configuration)).CodeBase).LocalPath, ".json");
+    private static readonly JsonSerializerSettings settings = new JsonSerializerSettings
+    {
+        ObjectCreationHandling = ObjectCreationHandling.Replace,
+        Converters = [
+            new StringEnumConverter()
+        ],
+        Formatting = Formatting.Indented,
+        Culture = CultureInfo.InvariantCulture
+    };
+    
+    #endregion
+    
+    #region Properties
+
+    #endregion
+    
+    #region Functions
+    
+    /// <summary>
+    /// Saves the configuration.
+    /// </summary>
+    public void Save()
+    {
+        string contents = JsonConvert.SerializeObject(this, settings);
+        File.WriteAllText(path, contents);
+    }
+    /// <summary>
+    /// Gets the current configuration.
+    /// </summary>
+    /// <returns>The current configuration, or a new configuration if is not present.</returns>
+    public static Configuration Load()
+    {
+        try
+        {
+            string contents = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<Configuration>(contents, settings);
+        }
+        catch (FileNotFoundException)
+        {
+            Configuration config = new Configuration();
+            config.Save();
+            return config;
+        }
+        catch (Exception e)
+        {
+            Notification.Show($"~r~Error~w~: Unable to load config: {e.Message}");
+            return new Configuration();
+        }
+    }
+    
+    #endregion
+}
---
+++ RGB4R/Script.cs
@@ -0,0 +1,39 @@
+﻿using GTA;
+using System;
+using System.Windows.Forms;
+
+namespace RGB4R;
+
+public class RGB4R : Script
+{
+    #region Fields
+    
+    private static readonly Configuration config = Configuration.Load();
+    
+    #endregion
+    
+    #region Constructors
+
+    public RGB4R()
+    {
+        Tick += OnTick;
+        KeyDown += OnKeyDown;
+        KeyUp += OnKeyUp;
+    }
+    
+    #endregion
+    
+    #region Event Functions
+
+    private void OnTick(object sender, EventArgs e)
+    {
+    }
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+    }
+    private void OnKeyUp(object sender, KeyEventArgs e)
+    {
+    }
+
+    #endregion
+}
