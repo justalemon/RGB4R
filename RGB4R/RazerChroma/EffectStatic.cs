c55597536ee42fbf5bc0a132fbdb6c1c51d15975
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-19 07:53:16 -0400

    Added class for the static effect

---
+++ RGB4R/RazerChroma/EffectStatic.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace RGB4R.RazerChroma;
+
+/// <summary>
+/// Represents the static effect.
+/// </summary>
+public class EffectStatic : Effect
+{
+    /// <summary>
+    /// The color to apply.
+    /// </summary>
+    public Color Color { get; set; }
+}
