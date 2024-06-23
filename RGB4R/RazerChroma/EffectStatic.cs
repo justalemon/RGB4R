142b435ef203d655083fe2524a56436929d6c0ec
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-23 08:14:33 -0400

    Added registration of the static effect

--- RGB4R/RazerChroma/EffectStatic.cs
+++ RGB4R/RazerChroma/EffectStatic.cs
@@ -1,4 +1,9 @@
 ﻿using System.Drawing;
+using Flurl;
+using Flurl.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RGB4R.Extensions;
 
 namespace RGB4R.RazerChroma;
 
@@ -7,8 +12,60 @@ namespace RGB4R.RazerChroma;
 /// </summary>
 public class EffectStatic : Effect
 {
+    #region Properties
+    
     /// <summary>
     /// The color to apply.
     /// </summary>
-    public Color Color { get; set; }
+    public Color Color { get; }
+    
+    #endregion
+    
+    #region Constructors
+
+    /// <summary>
+    /// Creates a new static effect.
+    /// </summary>
+    /// <param name="color">The color to use.</param>
+    public EffectStatic(Color color)
+    {
+        Color = color;
+    }
+    
+    #endregion
+    
+    #region Functions
+
+    /// <inheritdoc/>
+    public override void Register(Device device)
+    {
+        if (IsRegistered(device))
+        {
+            return;
+        }
+
+        JObject payload = new JObject
+        {
+            ["effect"] = "CHROMA_STATIC",
+            ["param"] = new JObject(),
+            ["param"] =
+            {
+                ["color"] = Color.ToBgr()
+            }
+        };
+
+        Url url = Chroma.Endpoint.Clone().AppendPathSegment(device.ToString().ToLower());
+        IFlurlResponse resp = url.PostJsonSync(payload);
+        string data = resp.GetStringSync();
+        RegistrationResult result = JsonConvert.DeserializeObject<RegistrationResult>(data);
+
+        if (result.Error != RazerError.Success)
+        {
+            throw new RazerException(result.Color, result.Error);
+        }
+        
+        Ids.Add(device, result.Id);
+    }
+    
+    #endregion
 }
