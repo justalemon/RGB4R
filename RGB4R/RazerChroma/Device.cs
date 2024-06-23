880f8488ef1cc4d968edd7d433634e29796ea8e4
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-23 07:27:59 -0400

    Added enum with all device names

---
+++ RGB4R/RazerChroma/Device.cs
@@ -0,0 +1,32 @@
+﻿namespace RGB4R.RazerChroma;
+
+/// <summary>
+/// The different types of devices allowed in Razer Chroma.
+/// </summary>
+public enum Device
+{
+    /// <summary>
+    /// Keyboards.
+    /// </summary>
+    Keyboard,
+    /// <summary>
+    /// Mouse, internally called Mice.
+    /// </summary>
+    Mouse,
+    /// <summary>
+    /// Headsets and Headphones.
+    /// </summary>
+    Headset,
+    /// <summary>
+    /// Mousepads.
+    /// </summary>
+    Mousepad,
+    /// <summary>
+    /// Macropads, internally called Keypads.
+    /// </summary>
+    Keypad,
+    /// <summary>
+    /// Devices connected via Chroma Link.
+    /// </summary>
+    ChromaLink
+}
