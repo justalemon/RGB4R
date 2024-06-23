6b546f5b9181091997906c4b36c3559cb8653205
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-23 06:56:27 -0400

    Added converter to RGBA

---
+++ RGB4R/Extensions/ColorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace RGB4R.Extensions;
+
+/// <summary>
+/// Extensions for converting colors between different types.
+/// </summary>
+public static class ColorExtensions
+{
+    /// <summary>
+    /// Converts a <see cref="Color"/> to BGR.
+    /// </summary>
+    /// <param name="color">The color to convert.</param>
+    /// <returns>The color converted to BGR as an integer.</returns>
+    public static int ToBgr(this Color color)
+    {
+        return (color.B << 16) + (color.G << 8) + color.R;
+    }
+}
