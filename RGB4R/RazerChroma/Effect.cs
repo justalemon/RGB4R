477e3c160738d234acfdee31e1cf32bf2133ff4d
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-23 07:28:30 -0400

    Added some properties and abstract functions to Effects

--- RGB4R/RazerChroma/Effect.cs
+++ RGB4R/RazerChroma/Effect.cs
@@ -1,8 +1,31 @@
-﻿namespace RGB4R.RazerChroma;
+﻿using System;
+
+namespace RGB4R.RazerChroma;
 
 /// <summary>
 /// Represents an effect that can be sent to a Chroma device.
 /// </summary>
 public abstract class Effect
 {
+    #region Properties
+
+    /// <summary>
+    /// The ID of this effect, if registered with Razer Chroma.
+    /// </summary>
+    public Guid Id { get; protected set; }
+    /// <summary>
+    /// Whether the effect is registered or not.
+    /// </summary>
+    public bool IsRegistered => Id != Guid.Empty;
+
+    #endregion
+    
+    #region Functions
+    
+    /// <summary>
+    /// Registers this effect with Razer.
+    /// </summary>
+    public abstract void Register();
+
+    #endregion
 }
