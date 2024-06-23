a132eddabc22549235708c34818f9c411a656a75
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-23 08:12:11 -0400

    Added class to get the resulting response from registration

---
+++ RGB4R/RazerChroma/RegistrationResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RGB4R.RazerChroma;
+
+/// <summary>
+/// The data sent back after registering an effect.
+/// </summary>
+public class RegistrationResult
+{
+    /// <summary>
+    /// A message related to the color.
+    /// </summary>
+    [JsonProperty("color")]
+    public string Color { get; set; }
+    /// <summary>
+    /// Any error code returned by the call.
+    /// </summary>
+    [JsonProperty("result")]
+    public RazerError Error { get; set; }
+    /// <summary>
+    /// The ID returned, if successful.
+    /// </summary>
+    [JsonProperty("id")]
+    public Guid Id { get; set; }
+}
