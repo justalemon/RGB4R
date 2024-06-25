0ec7415b757be7b42851824f4dc2fd78520431a0
Hannele Ruiz <justlemoncl@gmail.com>
2024-06-25 04:53:27 -0400

    Added loading of the default game colors

--- RGB4R/Script.cs
+++ RGB4R/Script.cs
@@ -1,6 +1,8 @@
 ﻿using GTA;
 using System;
+using System.Drawing;
 using System.Windows.Forms;
+using GTA.Native;
 using RGB4R.RazerChroma;
 
 namespace RGB4R;
@@ -9,6 +11,11 @@ public class RGB4R : Script
 {
     #region Fields
     
+    private readonly EffectStatic colorMichael = GetEffectFromGameColor(153);
+    private readonly EffectStatic colorFranklin = GetEffectFromGameColor(154);
+    private readonly EffectStatic colorTrevor = GetEffectFromGameColor(155);
+    private readonly EffectStatic colorFreemode = GetEffectFromGameColor(123);
+
     private static readonly Configuration config = Configuration.Load();
     
     #endregion
@@ -24,7 +31,26 @@ public class RGB4R : Script
     }
 
     #endregion
-    
+
+    #region Tools
+
+    private static EffectStatic GetEffectFromGameColor(int id)
+    {
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        int a = 0;
+        unsafe
+        {
+            Function.Call(Hash.GET_HUD_COLOUR, id, &r, &g, &b, &a);
+        }
+
+        Color color = Color.FromArgb(255, r, g, b);
+        return new EffectStatic(color);
+    }
+
+    #endregion
+
     #region Event Functions
 
     private void OnInit(object sender, EventArgs e)
